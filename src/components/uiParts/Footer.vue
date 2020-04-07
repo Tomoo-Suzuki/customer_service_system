7a030c1618a823efd5903bd57ac42d803fb6ee42
Tomoo Suzuki <solosoprani2002@gmail.com>
2020-04-07 18:44:13 +0900

    update header and footer components

--- src/components/uiParts/Footer.vue
+++ src/components/uiParts/Footer.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>
-    <h2>Footer</h2>
-  </div>
+  <footer>
+    <div>Topへ戻る</div>
+    <div>
+      <div>
+        <div>Logo-BookUpload</div>
+        <div>logo-BookSales</div>
+        <div>logo-BookRead</div>
+      </div>
+      <div>
+        <ul>
+          <li>ご利用規約</li>
+          <li>個人情報保護方針</li>
+          <li>特定商取引法に基づく表記</li>
+          <li>企業情報（採用）</li>
+          <li>ヘルプ・お問い合わせ</li>
+        </ul>
+      </div>
+    </div>
+    <div>copyright</div>
+  </footer>
 </template>
 <script>
 export default {
--- src/components/uiParts/Header.vue
+++ src/components/uiParts/Header.vue
@@ -1,7 +1,41 @@
 <template>
-  <div>
-    <h2>Header</h2>
-  </div>
+  <header>
+    <div>
+      <div>Logo-BookUpload</div>
+      <div>
+        <label><input type="text" /><button>検索</button></label>
+      </div>
+      <div>logo-BookSales</div>
+      <div>logo-BookRead</div>
+    </div>
+    <div>
+      <ul>
+        <li>ご注文履歴・ご返品</li>
+        <li>ポイント</li>
+        <li>カート</li>
+        <li>欲しいものリスト</li>
+        <li>マイページ</li>
+        <li>ヘルプ</li>
+        <li>アカウントサービス</li>
+      </ul>
+    </div>
+    <nav>
+      <ul>
+        <li>異世界ファンタジー</li>
+        <li>現代ファンタジー</li>
+        <li>SF</li>
+        <li>恋愛</li>
+        <li>ラブコメ</li>
+        <li>現代ドラマ</li>
+        <li>ホラー</li>
+        <li>ミステリー</li>
+        <li>エッセイ・ノンフィクション</li>
+        <li>歴史・時代・伝記</li>
+        <li>創作論・評論</li>
+        <li>詩・童話・その他</li>
+      </ul>
+    </nav>
+  </header>
 </template>
 <script>
 export default {
