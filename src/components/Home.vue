e06a58ad40d246fa97012f4cbcb3e77c0efa3851
Tomoo Suzuki <solosoprani2002@gmail.com>
2020-04-07 09:48:23 +0900

    update view router of js location

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,19 @@
 <template>
   <main>
     <h1>home</h1>
+    <button @click="toEachPage('/form-user')">User</button>
+    <button @click="toEachPage('/form-post')">Story Upload</button>
+    <button @click="toEachPage('/form-story')">Story Detail</button>
+    <button @click="toEachPage('/form-account')">Account</button>
   </main>
 </template>
+
+<script>
+export default {
+  methods: {
+    toEachPage(path) {
+      this.$router.push(path);
+    },
+  },
+};
+</script>
