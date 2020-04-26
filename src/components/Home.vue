9692893c7752bb2523357a073e09ad1fb4dfa022
Tomoo Suzuki <solosoprani2002@gmail.com>
2020-04-26 18:07:05 +0900

    graphql test

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,15 +5,31 @@
     <button @click="toEachPage('/form-post')">Story Upload</button>
     <button @click="toEachPage('/form-story')">Story Detail</button>
     <button @click="toEachPage('/form-account')">Account</button>
+    <button @click="getData">GQL</button>
   </main>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   methods: {
     toEachPage(path) {
       this.$router.push(path);
     },
-  },
+    getData() {
+      const query =
+        'query{user(id:"00001"){password,user_type,magazine,agree_to_terms,date_reception}}';
+      const url = "http://localhost:7777/graphql";
+      axios
+        .post(url, query)
+        .then(function(res) {
+          console.log(res);
+        })
+        .catch(function(e) {
+          console.log("反映テストその２");
+          console.log(e);
+        });
+    }
+  }
 };
 </script>
