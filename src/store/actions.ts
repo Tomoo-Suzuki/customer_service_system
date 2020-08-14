dcde6ac29f597f5ff818a669c3eda62b3d2f31a4
Tomoo Suzuki <solosoprani2002@gmail.com>
2020-08-14 12:04:06 +0900

    apply ts type to vuex

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -3,29 +3,34 @@ import { InterfaceUser } from "../types/InterfaceUser";
 import { InterfacePost } from "../types/InterfacePost";
 import { InterfaceStory } from "../types/InterfaceStory";
 
+import { ActionContext, Commit } from "/Use/node_modules/vuex/types/index";
+
 export default {
-  updateAccount(context: any, newAccount: InterfaceAccount) {
+  updateAccount(context: ActionContext, newAccount: InterfaceAccount) {
     context.commit("updateAccount", newAccount);
   },
-  updateFormAccount({ commit }: { commit: any }, eachForm: InterfaceAccount) {
+  updateFormAccount(
+    { commit }: { commit: Commit },
+    eachForm: InterfaceAccount
+  ) {
     commit("updateFormAccount", eachForm);
   },
-  updateLogin(context: any, newLogin: InterfaceUser) {
+  updateLogin(context: ActionContext, newLogin: InterfaceUser) {
     context.commit("updateLogin", newLogin);
   },
-  updateStory({ commit }: { commit: any }, newStory: InterfaceStory) {
+  updateStory({ commit }: { commit: Commit }, newStory: InterfaceStory) {
     commit("updateStory", newStory);
   },
-  updatePost({ commit }: { commit: any }, newPost: InterfacePost) {
+  updatePost({ commit }: { commit: Commit }, newPost: InterfacePost) {
     commit("updatePost", newPost);
   },
-  memoryForm({ commit }: { commit: any }, fromData: any) {
+  memoryForm({ commit }: { commit: Commit }, fromData: any) {
     commit("memoryForm", fromData);
   },
-  initErrorAndFlag({ commit }: { commit: any }, initErrorAndFlag: any) {
+  initErrorAndFlag({ commit }: { commit: Commit }, initErrorAndFlag: any) {
     commit("initErrorAndFlag", initErrorAndFlag);
   },
-  updateErrorMsg({ commit }: { commit: any }, error: any) {
+  updateErrorMsg({ commit }: { commit: Commit }, error: any) {
     commit("updateErrorMsg", error);
   },
 };
